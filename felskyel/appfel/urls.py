f6eee023d61373ae2a7e057b3b0692b898eb1011
pablodavidj862-prog <pablodavidj862@gmail.com>
2026-03-03 13:26:53 -0500

    jp
    Reordenar la sección y la paginas de contacto.

--- felskyel/appfel/urls.py
+++ felskyel/appfel/urls.py
@@ -6,8 +6,8 @@ urlpatterns = [
     path('shop/', views.shop, name='Tienda'),
     path('perfil/', views.perfil, name='perfil'),
     path('registro/', views.registro, name='registro'),
-    path('contacto/', views.contacto, name='contacto'),
-    path('prueba/', views.prueba, name='prueba'),
+    path('contactos-proveedor/', views.contacto, name='contacto'),
+    path('prueba2/', views.prueba2, name='prueba2'),
     path('solicitud-proveedor/', views.solicitud_proveedor, name='solicitud_proveedor'),
     path('terminos-y-condiciones/', views.terminos_y_condiciones, name='terminos_y_condiciones'),
 ]
