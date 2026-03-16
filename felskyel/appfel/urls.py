9d7a14613f46e6b6830670f0b011bf9ca1e33bfc
jp_juan <pablodavidj862@gmail.com>
2026-03-16 13:21:15 -0500

    jp
    cambios

--- felskyel/appfel/urls.py
+++ felskyel/appfel/urls.py
@@ -14,4 +14,8 @@ urlpatterns = [
     path('panel-control/', views.panel, name='panel'),
     path('perfil_contacto/', views.perfil_contacto, name='perfil_contacto'),
     path('admin_productos/', views.admin_productos, name='admin_productos'),
+    path('password_reset_request/', views.password_reset_request, name='password_reset_request'),
+    path('password_reset_done/', views.password_reset_done, name='password_reset_done'),
+    path('password_reset_complete/', views.password_reset_complete, name='password_reset_complete'),
+    path('password_reset_confirm/', views.password_reset_confirm, name='password_reset_confirm'),
 ]
--- felskyel/appfel/views.py
+++ felskyel/appfel/views.py
@@ -37,6 +37,18 @@ def perfil_contacto(request):
 def admin_productos(request):
     return render (request, 'panel_control/admin_productos.html')
 
+def password_reset_request(request):
+    return render (request, 'rest_passw/password_reset_request.html')
+
+def password_reset_done(request):
+    return render (request, 'rest_passw/password_reset_done.html')
+
+def password_reset_confirm(request):
+    return render (request, 'rest_passw/password_reset_confirm.html')
+
+def password_reset_complete(request):
+    return render (request, 'rest_passw/password_reset_complete.html')
+
 
 def solicitud_proveedor(request):
     if request.method == 'POST':
