b647fad634454a60d326a941746df382e0169da7
pablodavidj862-prog <pablodavidj862@gmail.com>
2026-03-22 21:12:11 -0500

    jp
    cambios en login y mostrar contraseña

--- felskyel/appfel/static/appfel/js/script.js
+++ felskyel/appfel/static/appfel/js/script.js
@@ -1,15 +1,15 @@
 
-function togglePassword(field) {
+// Mostrar/ocultar contraseña
+function togglePassword(fieldId) {
     const field = document.getElementById(fieldId);
     const icon = document.getElementById('icon-' + fieldId);
-    if (field.type === 'password') {
-        field.type = 'text';
+    if (field.type === "password") {
+        field.type = "text";
         icon.classList.remove('fa-eye');
         icon.classList.add('fa-eye-slash');
     } else {
-        field.type = 'password';
+        field.type = "password";
         icon.classList.remove('fa-eye-slash');
         icon.classList.add('fa-eye');
     }
-
 }
