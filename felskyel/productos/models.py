aab5c69c976425a5ae0d5f2382d794429ff1f5a1
pablodavidj862-prog <pablodavidj862@gmail.com>
2026-04-08 22:32:45 -0500

    Cambios y correcciones en la imagenes
    XD

--- felskyel/productos/models.py
+++ felskyel/productos/models.py
@@ -7,10 +7,14 @@ class Producto(models.Model):
     nombre = models.CharField(max_length=100)
     precio = models.DecimalField(max_digits=10, decimal_places=0)
     descripcion = models.TextField()
-    imagen = models.CharField(max_length=200, default= "default.png")
+    imagen = models.ImageField(upload_to='proveedores/imagenes/', null=True, blank=True)
 
     def __str__(self):
         return self.nombre
+    
+    def delete(self, using=None, keep_parents=False):
+        self.imagen.delete(self.imagen.name)
+        super().delete()
 
 class Comentario(models.Model):
     producto = models.ForeignKey(Producto, on_delete=models.CASCADE, related_name='comentarios', null=True, blank=True)
