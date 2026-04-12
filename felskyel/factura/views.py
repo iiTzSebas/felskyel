23119ea0b1f4c29a3b830225b61e9ecfa835e5bc
iiTzSebas <jtoscanoprieto@hotmail.com>
2026-04-11 23:42:44 -0500

    modo oscuro mejorado

--- felskyel/factura/views.py
+++ felskyel/factura/views.py
@@ -10,7 +10,7 @@ from productos.models import Producto
 
 # --- CONFIGURACIÓN DE STRIPE ---
 # ⚠️ CADA INTEGRANTE DEBE PEGAR SU PROPIA LLAVE AQUÍ PARA QUE FUNCIONE
-stripe.api_key = "PEGA_AQUÍ_TU_LLAVE_DE_STRIPE" 
+stripe.api_key = "pk_test_51TLFjUHr3uj7vgffJwATBNsRGLApYzw6KKlfApH2eUiwAGPH6xI9HBEit5WJzM75zsFHixTQkmdTsd9E6Db9VGXq00o70MzGx2" 
 # -------------------------------
 @login_required
 def crear_checkout(request):
